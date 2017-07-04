3831550b7cecdc095592a98d976fc526b0eb7fe3
Sunjay Varma <varma.sunjay@gmail.com>
2017-07-04 01:07:45 -0400

    Pooling requests and limiting to only a few at a time with a delay to avoid triggering GitHub's abuse detection system

--- github.js
+++ github.js
@@ -2,6 +2,11 @@ const fs = require('fs-extra');
 const request = require('request-promise');
 
 const ENDPOINT = 'https://api.github.com/graphql';
+// The limit on the number of concurrent requests (so GitHub doesn't think we're spamming)
+const CONNECTION_LIMIT = 10;
+// Avoid everything failing and GitHub thinking you're abusing the API by adding
+// a delay between requests
+const REQUEST_DELAY = 2000; // ms
 
 const PULL_REQUEST_QUERY = fs.readFileSync('pullRequest.graphql').toString();
 const PULL_REQUEST_LIST_QUERY = fs.readFileSync('pullRequestList.graphql').toString();
@@ -10,33 +15,34 @@ const PULL_REQUEST_LIST_QUERY = fs.readFileSync('pullRequestList.graphql').toStr
  * Fetch all pull requests rapidly in parallel
  */
 exports.fetchPullRequestList = (token) => {
+  const pool = new QueryPool(token);
   const fetchPage = ({data: {repository: {pullRequests}}}) => {
     const {totalCount, pageInfo, nodes} = pullRequests;
 
     console.info(`Downloading ${totalCount} pull requests...`);
     let nextPage = Promise.resolve([]);
     if (pageInfo.hasNextPage) {
-      nextPage = queryPullRequestList(token, pageInfo.endCursor).then(fetchPage);
+      nextPage = queryPullRequestList(pool, pageInfo.endCursor).then(fetchPage);
     }
 
     const completed = Promise.all(nodes.map(({number, title}) => {
       console.info(`Downloading PR #${number} ${title}`);
-      return fetchPullRequest(token, number);
+      return fetchPullRequest(pool, number);
     }));
 
     return nextPage.then((page) => completed.then((items) => items.concat(page)));
   };
 
-  return queryPullRequestList(token).then(fetchPage);
+  return queryPullRequestList(pool).then(fetchPage);
 };
 
-function fetchPullRequest(token, number) {
+function fetchPullRequest(pool, number) {
   const fetchPage = ({data: {repository: {pullRequest}}}) => {
     const {pageInfo} = pullRequest.timeline;
 
     let nextPage = Promise.resolve({timeline: {nodes: []}});
     if (pageInfo.hasNextPage) {
-      nextPage = queryPullRequest(token, number, pageInfo.endCursor).then(fetchPage);
+      nextPage = queryPullRequest(pool, number, pageInfo.endCursor).then(fetchPage);
     }
 
     return nextPage.then(({timeline}) => {
@@ -45,36 +51,67 @@ function fetchPullRequest(token, number) {
     });
   };
 
-  return queryPullRequest(token, number).then(fetchPage);
+  return queryPullRequest(pool, number).then(fetchPage);
 }
 
-function queryPullRequestList(token, after = undefined) {
-  return queryAPI(token, PULL_REQUEST_LIST_QUERY
+function queryPullRequestList(pool, after = undefined) {
+  return pool.query(PULL_REQUEST_LIST_QUERY
     .replace('$PAGEINATION', pagination({pageSize: 100, after})));
 }
 
-function queryPullRequest(token, number, after = undefined) {
-  return queryAPI(token, PULL_REQUEST_QUERY
+function queryPullRequest(pool, number, after = undefined) {
+  return pool.query(PULL_REQUEST_QUERY
     .replace('$PR_NUMBER', number)
     .replace('$PAGEINATION', pagination({pageSize: 100, after})));
-}
-
-function queryAPI(token, query) {
-  return request.post({
-    url: ENDPOINT,
-    auth: {
-      bearer: token,
-    },
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
-    },
-    body: {query: query.toString()},
-    json: true,
-  });
 }
 
 function pagination({pageSize, after = null}) {
   const first = `first: ${pageSize}`;
   const afterRef = after ? `, after: "${after}"` : '';
   return first + afterRef;
+}
+
+class QueryPool {
+  constructor(token) {
+    this.token = token;
+    this.queue = [];
+    this.requests = 0;
+  }
+
+  query(query) {
+    return new Promise((resolve, reject) => {
+      this.queue.push({query, resolve, reject});
+      this._dispatchNext();
+    });
+  }
+
+  _sendQuery({query, resolve, reject}) {
+    this.requests += 1;
+    request.post({
+      url: ENDPOINT,
+      auth: {
+        bearer: this.token,
+      },
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+      },
+      body: {query},
+      json: true,
+    }).then((result) => {
+      this.requests -= 1;
+      this._dispatchNext();
+      return result;
+    }).catch((err) => {
+      this.requests -= 1;
+      this._dispatchNext();
+      return err;
+    }).then(resolve).catch(reject);
+  }
+
+  _dispatchNext() {
+    if (this.requests < CONNECTION_LIMIT && this.queue.length > 0) {
+      setTimeout(() => this._sendQuery(this.queue.shift()), REQUEST_DELAY);
+      ;
+    }
+  }
 }
